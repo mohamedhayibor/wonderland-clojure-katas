72dcb86b59aaecd4ca5aab45997419aed946eca0
Mohamed Hayibor <mohamedhayibor@gmail.com>
2017-08-06 08:42:36 -0400

    add encoding alg

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -3,7 +3,6 @@
 (def alphabet-chars
   (mapv char (range 97 (+ 97 26))))
 
-
 (def alphabet
   (mapv str alphabet-chars))
 
@@ -12,14 +11,12 @@
   [coll]
   (vec (concat (rest coll) (vector (first coll)))))
 
-
 (defn alphabet-cipher
   "Takes alphabet then returns the 2d array representation of cipher"
   [coll]
   (vec (take 26 (iterate shift-by-one coll))))
 
-
-(defn seed-msg-seq
+(defn seed-msg-letter-set
   "Takes a seed (keyword) and a msg, sequential inputs for
   encryption and decryption.
   Note: the letter seed is always first."
@@ -27,26 +24,30 @@
   ;; Todo: will be more specific about the string length later,
   ;; works for now
   (if (> (count seed) (count msg))
-    (map str seed (apply str (repeat (count seed) msg)))
-    (map str (apply str (repeat (count msg) seed)) msg)))
-
+    (mapv str seed (apply str (repeat (count seed) msg)))
+    (mapv str (apply str (repeat (count msg) seed)) msg)))
 
 (defn char-encoding
   "Takes a letter seed and message and returns char encoding"
   [char-s char-m]
+  (get-in (alphabet-cipher alphabet) [(- (int char-m) 97)  (- (int char-s) 97)]))
 
-  (get-in (alphabet-cipher alphabet) [(- (int char-m) 97)  (- (int char-s) 97)]) )
 
 
-(defn encode [keyword message]
-  "encodeme")
+(defn encode
+  "encrypts letter by letter using keyword and message"
+  [seed msg]
+  ;; todo: refactor for threading
+  (apply str (map #(char-encoding (first %) (second %)) (map seq (seed-msg-letter-set seed msg)))))
 
-(defn decode [keyword message]
-  "decodeme")
+(defn char-decoding
+  "Takes a seed and cypher char and returns char message"
+  [char-s char-c]
+
+)
 
 (defn decipher [cipher message]
   "decypherme")
 
-; letter debugg
 (def letter-debugging
   (map (juxt char identity #(- % 97)) (range 97 (+ 97 26))))
