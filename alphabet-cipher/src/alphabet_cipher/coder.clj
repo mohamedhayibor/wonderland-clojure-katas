1efc5d4160af474d0ef8121fa191febfbfa83410
Mohamed Hayibor <mohamedhayibor@gmail.com>
2017-08-06 02:42:04 -0400

    fix shift-by-one

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -3,22 +3,24 @@
 (def alphabet-chars
   (mapv char (range 97 (+ 97 26))))
 
+
 (def alphabet
   (mapv str alphabet-chars))
 
 (defn shift-by-one
   "takes the last and insert at the begginning shifting everything."
   [coll]
-  (vec (concat (vector (peek coll)) (butlast coll))))
+  (vec (concat (rest coll) (vector (first coll)))))
 
 
 (defn alphabet-cipher
   "Takes alphabet then returns the 2d array representation of cipher"
   [coll]
-  (take 28 (iterate shift-by-one coll)))
+  (vec (take 26 (iterate shift-by-one coll))))
 
-(defn seed-and-msg
-  "Takes a seed (keyword) and a msg, then returns the inputs for
+
+(defn seed-msg-seq
+  "Takes a seed (keyword) and a msg, sequential inputs for
   encryption and decryption.
   Note: the letter seed is always first."
   [seed msg]
@@ -28,13 +30,12 @@
     (map str seed (apply str (repeat (count seed) msg)))
     (map str (apply str (repeat (count msg) seed)) msg)))
 
+
 (defn char-encoding
   "Takes a letter seed and message and returns char encoding"
   [char-s char-m]
-  (let [x (int char-s)
-        y (int char-m)
-        cipher (alphabet-cipher alphabet)]
-   (get-in cipher [(- y 97)  (- x 97)])))
+
+  (get-in (alphabet-cipher alphabet) [(- (int char-m) 97)  (- (int char-s) 97)]) )
 
 
 (defn encode [keyword message]
@@ -45,3 +46,7 @@
 
 (defn decipher [cipher message]
   "decypherme")
+
+; letter debugg
+(def letter-debugging
+  (map (juxt char identity #(- % 97)) (range 97 (+ 97 26))))
